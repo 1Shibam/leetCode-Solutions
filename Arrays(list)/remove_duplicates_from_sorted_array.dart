bd108f41575bd1899c527ac90143086e7acbb552
1Shibam <hehe.shibam@gmail.com>
2024-12-21 14:40:54 +0530

    add alternate solution to this

--- Arrays(list)/remove_duplicates_from_sorted_array.dart
+++ Arrays(list)/remove_duplicates_from_sorted_array.dart
@@ -1,12 +1,22 @@
 class Solution {
   int removeDuplicates(List<int> nums) {
     int count = 0;
-    for(int i = 1; i < nums.length; i++) {
-        if(nums[i - 1] != nums[i]) {
-            count++;
-            nums[count] = nums[i];
-        }
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i - 1] != nums[i]) {
+        count++;
+        nums[count] = nums[i];
+      }
     }
     return count + 1;
+  }
+}
+//Alternate solutions
+
+class AlterSolution {
+  int removeDuplicates(List<int> nums) {
+    List<int> newL = List.from(nums.toSet());
+    nums.clear();
+    nums.addAll(newL);
+    return newL.length;
   }
 }
