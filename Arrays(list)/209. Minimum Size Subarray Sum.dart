9227934cc439b74e943ae0deb4811987f7a814ac
1Shibam <hehe.shibam@gmail.com>
2025-01-07 15:12:59 +0530

    leetCode 209- Minimum size subarray sum

---
+++ Arrays(list)/209. Minimum Size Subarray Sum.dart
@@ -0,0 +1,20 @@
+import 'dart:math';
+
+class Solution {
+  int minSubArrayLen(int target, List<int> nums) {
+    int left = 0;
+    int total = 0;
+    int resLen = 1<<63 - 1;
+    for(int right = 0; right < nums.length; right++){
+        total += nums[right];
+        while(total >= target){
+            resLen = min(resLen, right - left + 1);
+            total = total - nums[left];
+            left++;
+        }
+    }
+    if(resLen == 1<<63-1) return 0;
+    return resLen;
+    
+  }
+}
