5636388842d9b0b8ff2a29eb67de329be90cd483
1Shibam <hehe.shibam@gmail.com>
2025-01-12 10:26:18 +0530

    three easy solutons

--- 81. Search in Rotated Sorted Array II.dart
+++ 81. Search in Rotated Sorted Array II.dart
@@ -1,7 +1,26 @@
 class Solution {
   bool search(List<int> nums, int target) {
+    //solution one line
+
     // return nums.indexOf(target) != -1;
-    return nums.contains(target);
+
+    //another one line solution
+    // return nums.contains(target);
+
+    // i like to suffer more ahh solution
+    int left = 0;
+    int right = nums.length -1;
+    while(left <= right ){
+        if(nums[left] == target){
+            return true;
+        }
+        else if(nums[right] == target){
+            return true;
+        }
+        left++;
+        right--;
+    }
+    return false;
     
   }
 }
