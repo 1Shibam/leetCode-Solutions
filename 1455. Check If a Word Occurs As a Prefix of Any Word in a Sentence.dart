bb67066359e24e32f22c0ab6d199c974d25c9622
1Shibam <hehe.shibam@gmail.com>
2025-01-13 11:48:22 +0530

    1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.dart
@@ -0,0 +1,10 @@
+class Solution {
+  int isPrefixOfWord(String sentence, String searchWord) {
+    List<String> words = sentence.split(' ');
+    for(int i=0; i<words.length; i++){
+        if(words[i].indexOf(searchWord) == 0) return i+1;
+    }
+    return -1;
+    
+  }
+}
