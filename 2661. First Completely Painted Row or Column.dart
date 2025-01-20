7d008b7403660811d702ca716c1c1638407b74e9
1Shibam <hehe.shibam@gmail.com>
2025-01-20 11:33:13 +0530

    brute force

--- 2661. First Completely Painted Row or Column.dart
+++ 2661. First Completely Painted Row or Column.dart
@@ -84,3 +84,54 @@ class Solution2 {
     return -1;  // If no row or column is completely painted
   }
 }
+
+class Solution {
+  bool checkRowPainted(List<List<int>> mat, int row) {
+    for (int col = 0; col < mat[0].length; col++) {
+      if (mat[row][col] > 0) { // not painted
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool checkColPainted(List<List<int>> mat, int col) {
+    for (int row = 0; row < mat.length; row++) {
+      if (mat[row][col] > 0) { // not painted
+        return false;
+      }
+    }
+    return true;
+  }
+
+  int firstCompleteIndex3(List<int> arr, List<List<int>> mat) {
+    int m = mat.length;
+    int n = mat[0].length;
+
+    // Map to store value to cell-coordinate [i][j]
+    Map<int, List<int>> mp = {};
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        int val = mat[i][j];
+        mp[val] = [i, j];
+      }
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+      int val = arr[i];
+
+      var cell = mp[val]!;
+      int row = cell[0];
+      int col = cell[1];
+
+      mat[row][col] *= -1; // painted
+
+      if (checkRowPainted(mat, row) || checkColPainted(mat, col)) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+}
